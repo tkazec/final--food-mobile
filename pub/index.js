53f4a4684339c6de4fe0232ca121fab80bb0ab4d
Teddy Cross <tkazec@gmail.com>
2013-09-12 02:38:56 -0700

    add geolocation, mobile support

--- pub/index.js
+++ pub/index.js
@@ -1,6 +1,12 @@
-new GMaps({
+var map = new GMaps({
 	div: "#map-inner",
 	lat: 37.7749295,
 	lng: -122.4194155,
 	zoom: 13
+});
+
+GMaps.geolocate({
+	success: function (pos) {
+		map.setCenter(pos.coords.latitude, pos.coords.longitude);
+	}
 });
